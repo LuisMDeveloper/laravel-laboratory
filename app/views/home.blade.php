3693cb26200e9a12f91a79d6a77f1a5fa4a4eb55
Luis Manuel Ramirez Vargas <luis.manuel.ramirez.91@gmail.com>
2014-08-12 23:10:28 -0500

    tutorial 1: rutas plantillas y controladores

---
+++ app/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="jumbotron">
+    <h1>Hello, world!</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt inventore nemo possimus. Blanditiis eaque
+        error esse, facere incidunt iure mollitia non omnis quasi temporibus. A at dolore harum provident suscipit?</p>
+
+    <p><a class="btn btn-primary btn-lg" role="button">Learn more</a></p>
+</div>
+@stop
